dbf341530172f23a103713aed9aaf9726f770f73
Cave-Johnson10 <30587387+Cave-Johnson10@users.noreply.github.com>
2019-11-30 21:37:11 -0600

    Add files via upload
    Added a config option for the plugin. Added a new "cancel" option.

--- src/me/Kurumi/ArmorStandFix.java
+++ src/me/Kurumi/ArmorStandFix.java
@@ -1,12 +1,23 @@
 package me.Kurumi;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ArmorStandFix extends JavaPlugin {
+FileConfiguration config = this.getConfig();
+
+
 
     public void onEnable(){
+        this.saveDefaultConfig();
+        config.addDefault("Type", "rename");
         Bukkit.getPluginManager().registerEvents(new OnInteract(), this);
 
+
+    }
+    public FileConfiguration getAConfig(){
+        return config;
     }
 }
--- src/me/Kurumi/OnInteract.java
+++ src/me/Kurumi/OnInteract.java
@@ -1,27 +1,56 @@
 package me.Kurumi;
 
-import net.minecraft.server.v1_14_R1.EntityArmorStand;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import me.Kurumi.ArmorStandFix;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 
 public class OnInteract implements Listener {
+    Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("ArmorStandFix");
+    FileConfiguration config = plugin.getConfig();
 
     @EventHandler
-    public void OnInteract(PlayerInteractAtEntityEvent event){
+    public void OnInteract(PlayerInteractEntityEvent event){
+
         Player p = event.getPlayer();
         Entity e = event.getRightClicked();
-        if(p.getInventory().getItemInMainHand().getType() == Material.NAME_TAG){
-            System.out.println("NAME TAG IF SUCCESS");
+        PlayerInventory inv = p.getInventory();
+        if(inv.getItemInMainHand().getType() == Material.NAME_TAG){
                 if(e instanceof ArmorStand){
-                    System.out.println("IS ARMORSTAND");
-                String n = p.getInventory().getItemInMainHand().getItemMeta().getDisplayName();
-                e.setCustomNameVisible(true);
-                e.setCustomName(n);
+                 if(config.getString("Type").equalsIgnoreCase("rename")) {
+                     String n = inv.getItemInMainHand().getItemMeta().getDisplayName();
+                     e.setCustomNameVisible(true);
+                     e.setCustomName(n);
+                 }
+                 if(config.getString("Type").equalsIgnoreCase("cancel")){
+                    // String n = e.getCustomName();
+                    // e.setCustomName(n);
+
+
+                     //ItemStack nameTag = inv.getItemInMainHand();
+                     //ItemMeta metaNameTag = nameTag.getItemMeta();
+                    // metaNameTag.setDisplayName(p.getInventory().getItemInMainHand().getItemMeta().getDisplayName());
+                    // inv.remove(Material.NAME_TAG);
+                     event.setCancelled(true);
+                     //inv.addItem(nameTag);
+
+
+
+
+                 }
             }
         }
 
